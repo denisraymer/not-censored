af53a5d1b573fca0348add5db0bf31402e4a47cb
DenisRaymer <to.mars.66.miles@gmail.com>
2020-06-23 13:34:53 +0300

    Updates

---
+++ precache-manifest.5c8410b64cb83ee06499eb0bdacaf91d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b344d72705a42b5b9bd01cb305aad68",
+    "url": "/font-presentation/index.html"
+  },
+  {
+    "revision": "f960cde133907bb347f7",
+    "url": "/font-presentation/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e98083b655895cbcf0a3",
+    "url": "/font-presentation/static/css/main.14de15e3.chunk.css"
+  },
+  {
+    "revision": "f960cde133907bb347f7",
+    "url": "/font-presentation/static/js/2.61ca55d0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/font-presentation/static/js/2.61ca55d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98083b655895cbcf0a3",
+    "url": "/font-presentation/static/js/main.71dabf96.chunk.js"
+  },
+  {
+    "revision": "9a20ff5c2f9b71449d86",
+    "url": "/font-presentation/static/js/runtime-main.119592b2.js"
+  },
+  {
+    "revision": "5b972d739418ff7a17e60833872203a0",
+    "url": "/font-presentation/static/media/FreeSans.5b972d73.ttf"
+  },
+  {
+    "revision": "55529b248297a63ba84e516608241db3",
+    "url": "/font-presentation/static/media/HelveticaRegular.55529b24.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/font-presentation/static/media/HelveticaRegular.d41d8cd9.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/font-presentation/static/media/HelveticaRegular.d41d8cd9.woff"
+  },
+  {
+    "revision": "9405f4239727d6009b67bd915dc4f2fa",
+    "url": "/font-presentation/static/media/copyright.9405f423.svg"
+  }
+]);
