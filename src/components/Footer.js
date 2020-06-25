514f1c41614c8c90597049dd128a1ecbdb411267
DenisRaymer <to.mars.66.miles@gmail.com>
2020-06-25 21:14:31 +0300

    Added marquee

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react';
+import MarqueeText from 'react-marquee-text-component';
 
 const Footer = () => {
+    let text = 'ВНИМАНИЕ! САЙТ СОДЕРЖИТ НЕНОРМАТИВНУЮ ЛЕКСИКУ (И ПОСВЯЩЕН ЕЙ) • 18+ • ВНИМАНИЕ! САЙТ СОДЕРЖИТ НЕНОРМАТИВНУЮ ЛЕКСИКУ (И ПОСВЯЩЕН ЕЙ)';
+
     return (
         <div className='footer'>
             <div className='marquee-stroke'>
-                <marquee behavior="alternate" direction="left" bgcolor="#fff">
-                    ВНИМАНИЕ! САЙТ СОДЕРЖИТ НЕНОРМАТИВНУЮ ЛЕКСИКУ (И ПОСВЯЩЕН ЕЙ) • 18+ • ВНИМАНИЕ! САЙТ СОДЕРЖИТ НЕНОРМАТИВНУЮ ЛЕКСИКУ (И ПОСВЯЩЕН ЕЙ)
-                </marquee>
+                <MarqueeText text={text} duration={40}/>
             </div>
         </div>
     )
