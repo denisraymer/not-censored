39bb9b0521ca1abc46b6a091dc558ec52ed92250
DenisRaymer <to.mars.66.miles@gmail.com>
2020-06-26 18:08:05 +0300

    Added store content

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -31,15 +31,39 @@ const Store = () => {
                 </div>
                 <div className='store__item'>
                     <img src={require('../assets/images/store/image_03.png')} alt=""/>
-                    <div className="store__title">ЛОНГСЛИВ ОХУЕНЧИК <br/> 1800.- Р</div>
+                    <div className="store__title">МАЙКА ОХУЕННО <br/> 2500.- Р</div>
                 </div>
                 <div className='store__item'>
                     <img src={require('../assets/images/store/image_04.png')} alt=""/>
-                    <div className="store__title">ЛОНГСЛИВ ОХУЕНЧИК <br/> 1800.- Р</div>
+                    <div className="store__title">ЛОНГСЛИВ ЗАЕБАЛСЯ <br/> 1800.- Р</div>
                 </div>
                 <div className='store__item'>
                     <img src={require('../assets/images/store/image_05.png')} alt=""/>
-                    <div className="store__title">ЛОНГСЛИВ ОХУЕНЧИК <br/> 1800.- Р</div>
+                    <div className="store__title">ЦЕПЬ ЁБА <br/> 3000.- Р</div>
+                </div>
+                <div className='store__item'>
+                    <img src={require('../assets/images/store/image_06.png')} alt=""/>
+                    <div className="store__title">ШАРФ ОХУЕННО <br/> 2000.- Р</div>
+                </div>
+                <div className='store__item'>
+                    <img src={require('../assets/images/store/image_07.png')} alt=""/>
+                    <div className="store__title">ШАРФ ОХУИТЕЛЬНО <br/> 2000.- Р</div>
+                </div>
+                <div className='store__item'>
+                    <img src={require('../assets/images/store/image_08.png')} alt=""/>
+                    <div className="store__title">ПАТЧ ХУЙ <br/> 500.- Р</div>
+                </div>
+                <div className='store__item'>
+                    <img src={require('../assets/images/store/image_09.png')} alt=""/>
+                    <div className="store__title">ПАТЧ БЛЯДЬ <br/> 500.- Р</div>
+                </div>
+                <div className='store__item'>
+                    <img src={require('../assets/images/store/image_10.png')} alt=""/>
+                    <div className="store__title">ПИН ПИЗДА <br/> 500.- Р</div>
+                </div>
+                <div className='store__item'>
+                    <img src={require('../assets/images/store/image_11.png')} alt=""/>
+                    <div className="store__title">ПИН ПИЗДА <br/> 500.- Р</div>
                 </div>
             </ScrollHorizontal>
         </div>
