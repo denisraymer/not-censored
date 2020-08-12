cdc2ed1646eaf73cc2fa304e682622757f58049b
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-12 13:28:46 +0300

    Create page for testing react hooks

---
+++ src/pages/Hooks.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react';
+import Container, {Col} from 'react-bootstrap';
+import Row from 'react-bootstrap';
+
+const Hooks = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Hooks
