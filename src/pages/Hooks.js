3e44b7b5fc9c743300fc96aef50ef0ba6ac79369
DenisRaymer <to.mars.66.miles@gmail.com>
2020-08-12 14:13:31 +0300

    UseState hook

--- src/pages/Hooks.js
+++ src/pages/Hooks.js
@@ -1,12 +1,45 @@
-import React, {useEffect, useState} from 'react';
-import {Container, Col, Row} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Container, Col, Row, Form, Button} from 'react-bootstrap';
 
 const Hooks = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        console.log('Data: ', email, password)
+    }
+
     return (
         <Container>
-            <Row>
-                <Col>
+            <Row className="justify-content-lg-center">
+                <Col xl={4}>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                value={email}
+                                placeholder="Enter email"
+                                onChange={event => setEmail(event.target.value)}/>
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                value={password}
+                                placeholder="Password"
+                                onChange={event => setPassword(event.target.value)}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
